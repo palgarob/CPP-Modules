2d43d03237f50fd5a59bcda182f982500e68025e
Pedro Palomares <pedpalgarob@gmail.com>
2024-05-27 21:35:33 +0200

    progress

---
+++ mo01/ex03/HumanB.hpp
@@ -0,0 +1,19 @@
+#ifndef HUMANB_HPP
+#define HUMANB_HPP
+
+#include <iostream>
+#include "Weapon.hpp"
+
+class HumanB
+{
+private:
+	Weapon*		_weapon;
+	std::string	_name;
+public:
+	HumanB(std::string name);
+	~HumanB(void);
+	void	attack(void);
+	void	setWeapon(Weapon &weapon);
+};
+
+#endif

27835d522e8c6478d5134d46896c4cac4b273bd1
Pedro Palomares <pedpalgarob@gmail.com>
2024-05-28 19:06:05 +0200

    progress

--- mo01/ex04/main.cpp
+++ mo01/ex04/main.cpp
@@ -7,13 +7,39 @@ int	main(int argc, char **argv)
 		std::cerr << "Wrong number of arguments" << std::endl;
 		return 1;
 	}
-	std::ifstream	ifs(argv[1]);
+
+	std::string	fileName = argv[1];
+	std::ifstream	ifs(fileName);
 	if (!ifs.is_open()) {
 		std::cerr << "Error opening file" << std::endl;
 		return 1;
 	}
+
+	std::string	before = argv[2];
+	std::string	after = argv[3];
+
 	std::string	text;
 	std::string	line;
 	while (getline(ifs, line))
+	{
 		text += line;
+		text += "\n";
+	}
+	ifs.close();
+
+	size_t	ret = 0;
+	do {
+		ret = text.find(argv[2], ret);
+		if (ret == std::string::npos)
+			break;
+		text.erase(ret, before.length());
+		text.insert(ret, after);
+	} while (1);
+
+	std::string	newFileName = fileName;
+	newFileName += ".replace";
+	std::ofstream	ofs(newFileName);
+	ofs << text << std::endl;
+	ofs.close();
+	return 0;
 }
---
+++ mo01/ex05/Harl.cpp
@@ -0,0 +1,31 @@
+#include "Harl.hpp"
+
+Harl::Harl() {}
+
+Harl::~Harl() {}
+
+void	Harl::debug(void) {
+	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I really do!" << std::endl;
+}
+
+void	Harl::info() {
+	std::cout << "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!" << std::endl;
+}
+
+void	Harl::warning() {
+	std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month." << std::endl;
+}
+
+void	Harl::error() {
+	std::cout << "This is unacceptable! I want to speak to the manager now." << std::endl;
+}
+
+void    Harl::complain( std:: string level ) {
+    harl_func_t  funcs[] = { &Harl::debug, &Harl::info, &Harl::warning, &Harl::error };
+    std::string levels[] = { "DEBUG", "INFO", "WARNING", "ERROR"};
+    int i = 0;
+    while (i < 4 && levels[i].compare(level))
+        i++ ;
+    if (i < 4)
+        (this->*funcs[i])();
+}

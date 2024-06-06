8377cc756e67171c4333e79d7b8532da4dbe1121
Pedro Palomares <pedpalgarob@gmail.com>
2024-06-06 16:06:15 +0200

    progress

---
+++ mo03/ex00/ClapTrap.cpp
@@ -0,0 +1,57 @@
+#include "ClapTrap.hpp"
+#include <iostream>
+
+ClapTrap::ClapTrap(std::string name) : _name(name), _health(10), _energy(10), _damage(0)
+{
+	std::cout << "ClapTrap " << name << " was created with the default constructor." << std::endl;
+}
+
+ClapTrap::ClapTrap(ClapTrap const & src)
+{
+	std::cout << "ClapTrap " << src._name << " was copied with the copy constructor." << std::endl;
+	*this = src;
+}
+
+ClapTrap &	ClapTrap::operator=(ClapTrap const & rhs)
+{
+	if (this != &rhs)
+	{
+		this->_name = rhs._name;
+		this->_health = rhs._health;
+		this->_energy = rhs._energy;
+		this->_damage = rhs._damage;
+	}
+	return *this;
+}
+
+ClapTrap::~ClapTrap() {}
+
+void	ClapTrap::attack(const std::string& target)
+{
+	if (this->_energy != 0)
+	{
+		std::cout << "ClapTrap " << this->_name << " attacks " << target << ", causing " << this->_damage << " points of damage!" << std::endl;
+		this->_energy -= 1;
+	}
+	else
+		std::cout << "ClapTrap " << this->_name << " can't attack because it has no energy left." << std::endl;
+}
+
+void	ClapTrap::takeDamage(unsigned int amount)
+{
+	if (amount > this->_health) this->_health = 0; else this->_health -= amount;
+	std::cout << "ClapTrap " << this->_name << " was dealt " << amount << " points of damage, leaving it with " << this->_health << " hit points!" << std::endl;
+}
+
+void	ClapTrap::beRepaired(unsigned int amount)
+{
+	if (this->_energy != 0)
+	{
+		this->_health += amount;
+		std::cout << "ClapTrap " << this->_name << " recovers " << amount << "hit points, leaving it with" << this->_health << std::endl;
+		this->_energy -= 1;
+	}
+	else
+		std::cout << "ClapTrap " << this->_name << " can't recover because it has no energy left." << std::endl;
+
+}
---
+++ mo03/ex00/ClapTrap.hpp
@@ -0,0 +1,25 @@
+#ifndef CLAPTRAP_HPP
+#define CLAPTRAP_HPP
+
+#include <string>
+
+class ClapTrap
+{
+private:
+	std::string		_name;
+	unsigned int	_health;
+	unsigned int	_energy;
+	unsigned int	_damage;
+public:
+	ClapTrap(std::string name);
+	ClapTrap(ClapTrap const & src);
+	ClapTrap &	operator=(ClapTrap const & rhs);
+	~ClapTrap();
+
+	void	attack(const std::string& target);
+	void	takeDamage(unsigned int amount);
+	void	beRepaired(unsigned int amount);
+};
+
+
+#endif

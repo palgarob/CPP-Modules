0c23fcfba1f16f557b08c8d37a14c3616c9d92e9
Pedro <pedpalgarob@gmail.com>
2025-03-20 22:37:15 +0000

    mo06/ex02

---
+++ mo06/ex02/Base.hpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Base.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pepaloma <pepaloma@student.42urduliz.com>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/20 03:15:43 by pepaloma          #+#    #+#             */
+/*   Updated: 2025/03/20 03:29:52 by pepaloma         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BASE_HPP
+#define BASE_HPP
+
+class Base {
+public:
+	virtual ~Base() {}
+};
+
+class A : public Base {};
+class B : public Base {};
+class C : public Base {};
+
+#endif // BASE_HPP
---
+++ mo06/ex02/main.cpp
@@ -0,0 +1,71 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pepaloma <pepaloma@student.42urduliz.com>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/20 11:59:29 by pepaloma          #+#    #+#             */
+/*   Updated: 2025/03/20 22:33:48 by pepaloma         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Base.hpp"
+#include <cstdlib>
+#include <iostream>
+
+Base* generate(void)
+{
+	int randomValue = rand() % 3;
+	if (randomValue == 0) return new A();
+	if (randomValue == 1) return new B();
+	return new C();
+}
+
+void identify(Base* p)
+{
+	if (dynamic_cast<A*>(p)) {
+		std::cout << "A" << std::endl;
+	} else if (dynamic_cast<B*>(p)) {
+		std::cout << "B" << std::endl;
+	} else if (dynamic_cast<C*>(p)) {
+		std::cout << "C" << std::endl;
+	} else {
+		std::cout << "Unknown type" << std::endl;
+	}
+}
+
+void identify(Base& p)
+{
+	try {
+		(void)dynamic_cast<A&>(p);
+		std::cout << "A" << std::endl;
+	} catch (std::exception&) {}
+
+	try {
+		(void)dynamic_cast<B&>(p);
+		std::cout << "B" << std::endl;
+	} catch (std::exception&) {}
+
+	try {
+		(void)dynamic_cast<C&>(p);
+		std::cout << "C" << std::endl;
+	} catch (std::exception&) {}
+}
+
+#include "Base.hpp"
+#include <iostream>
+
+int main() {
+	std::srand(time(0)); // Seed random generator
+
+	Base* obj = generate(); // Generate a random object
+	std::cout << "Identifying using pointer: ";
+	identify(obj);
+
+	std::cout << "Identifying using reference: ";
+	identify(*obj); // Pass as reference
+
+	delete obj; // Free memory
+	return 0;
+}

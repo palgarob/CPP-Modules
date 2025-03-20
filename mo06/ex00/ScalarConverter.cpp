17bc09a1037e575f51760aadf8a248e1a9eb6b42
Pedro <pedpalgarob@gmail.com>
2025-03-20 01:21:11 +0000

    fix mo06/ex00

--- mo06/ex00/ScalarConverter.cpp
+++ mo06/ex00/ScalarConverter.cpp
@@ -6,7 +6,7 @@
 /*   By: pepaloma <pepaloma@student.42urduliz.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/18 11:46:22 by pepaloma          #+#    #+#             */
-/*   Updated: 2025/03/18 20:51:11 by pepaloma         ###   ########.fr       */
+/*   Updated: 2025/03/20 01:19:07 by pepaloma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,61 +19,89 @@ void	impossible()
 {
 	std::cout << "char:		impossible" << std::endl;
 	std::cout << "int:		impossible" << std::endl;
-	std::cout << "float:	impossible" << std::endl;
-	std::cout << "double:	impossible" << std::endl;
+	std::cout << "float:		impossible" << std::endl;
+	std::cout << "double:		impossible" << std::endl;
+}
+
+void	output(int integer, float floating, double doublePrecision)
+{
+	if (integer < 33 || integer > 126)
+		std::cout << "char:		Non displayable" << std::endl;
+	else if (
+		integer > std::numeric_limits<char>::max()
+		|| integer < std::numeric_limits<char>::min()
+	)
+		std::cout << "char:		impossible" << std::endl;
+	else
+		std::cout << "char:		" << static_cast<char>(integer) << std::endl;
+	std::cout << "int:		" << integer << std::endl;
+	std::cout << "float:		" << floating 
+	<< (std::abs(floating - static_cast<int>(floating)) > 0 ? "" : ".0") << "f" << std::endl;
+	std::cout << "double:		" << doublePrecision
+	<< (std::abs(floating - static_cast<int>(floating)) > 0 ? "" : ".0") << std::endl;
 }
 
 void ScalarConverter::convert(const std::string& input)
 {
-	if (input.find('.', 0) != std::string::npos) {
-		char* endptr;
-		double num = strtod(input.c_str(), &endptr);
-		if (endptr == input.c_str()) {
-			impossible();
-			return ;
-		}
-		else if (
+	char* endptr;
+	double num = strtod(input.c_str(), &endptr);
+	if (endptr == input.c_str()) {
+		impossible();
+	}
+	else if (input.find('.', 0) != std::string::npos) {
+		if (
 			*endptr == 'f' && num >= std::numeric_limits<float>::min()
 			&& num <= std::numeric_limits<float>::max()
 		)
 		{
-			std::cout << "char:		" << static_cast<char>(num) << std::endl;
-			std::cout << "int:		" << static_cast<int>(num) << std::endl;
-			std::cout << "float:	" << num << std::endl;
-			std::cout << "double:	" << static_cast<double>(num) << std::endl;
+			output(
+				static_cast<int>(num),
+				num, static_cast<double>(num)
+			);
 		}
 		else
 		{
-			std::cout << "char:		" << static_cast<char>(num) << std::endl;
-			std::cout << "int:		" << static_cast<int>(num) << std::endl;
-			std::cout << "float:	" << static_cast<float>(num) << std::endl;
-			std::cout << "double:	" << num << std::endl;
+			output(
+				static_cast<int>(num),
+				static_cast<float>(num), num
+			);
 		}
+	}
+	else if (
+		!input.compare(0, 4, "-inf") || !input.compare(0, 4, "+inf")
+		|| !input.compare(0, 3, "nan")
+	)
+	{
+		std::cout << "char:		impossible" << std::endl;
+		std::cout << "int:		impossible" << std::endl;
+		std::cout << "float:	"
+		<< ('f' == (char)input.at(4) ? input.substr(0, 4) : input.substr(0, 3)) << "f" << std::endl;
+		std::cout << "double:	"
+		<< ((input.at(2) == 'i') ? input.substr(0, 4) : input.substr(0, 3)) << std::endl;
+	}
+	else if (
+		!input.compare(0, 5, "-inff") || !input.compare(0, 5, "+inff")
+		|| !input.compare(0, 4, "nanf")
+	)
+	{
+		std::cout << "char:		impossible" << std::endl;
+		std::cout << "int:		impossible" << std::endl;
+		std::cout << "float:	"
+		<< ((input.at(2) == 'i') ? input.substr(0, 5) : input.substr(0, 4)) << std::endl;
+		std::cout << "double:	"
+		<< ((input.at(2) == 'i') ? input.substr(0, 4) : input.substr(0, 3)) << std::endl;
 	}
 	else
 	{
 		int	num;
 		std::stringstream strs(input);
 		strs >> num;
-		if (
-			num <= std::numeric_limits<char>::max()
-			&& num >= std::numeric_limits<char>::min()
-		)
 		{
-			if (num >= 33 && num <=126)
-			{
-				std::cout << "char:		" << static_cast<char>(num) << std::endl;
-				std::cout << "int:		" << num << std::endl;
-				std::cout << "double:	" << static_cast<double>(num) << std::endl;
-				std::cout << "float:	" << static_cast<float>(num) << std::endl;
-			}
-			else
-			{
-				std::cout << "char:		Non-displayable" << std::endl;
-				std::cout << "int:		" << num << std::endl;
-				std::cout << "double:	" << static_cast<double>(num) << std::endl;
-				std::cout << "float:	" << static_cast<float>(num) << std::endl;
-			}
+			output(
+				num,
+				static_cast<float>(num),
+				static_cast<double>(num)
+			);
 		}
 	}
 }
--- mo06/ex00/main.cpp
+++ mo06/ex00/main.cpp
@@ -6,13 +6,14 @@
 /*   By: pepaloma <pepaloma@student.42urduliz.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/18 11:38:45 by pepaloma          #+#    #+#             */
-/*   Updated: 2025/03/18 20:49:25 by pepaloma         ###   ########.fr       */
+/*   Updated: 2025/03/19 22:00:21 by pepaloma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ScalarConverter.hpp"
 
 int	main(int argc, char **argv) {
-	(void)argc;
+	if (argc == 1)
+	return (1);
 	ScalarConverter::convert(argv[1]);
 }

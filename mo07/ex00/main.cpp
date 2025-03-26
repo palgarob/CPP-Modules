3a9dd3afe8a5a2fb7521a9c8933177b22f0beb25
Pedro <pedpalgarob@gmail.com>
2025-03-26 16:46:23 +0000

    mo07 ex00 ex01

---
+++ mo07/ex00/main.cpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pepaloma <pepaloma@student.42urduliz.com>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/26 16:27:56 by pepaloma          #+#    #+#             */
+/*   Updated: 2025/03/26 16:41:13 by pepaloma         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "swap.hpp"
+#include "min.hpp"
+#include "max.hpp"
+#include <iostream>
+
+int main( void ) {
+int a = 2;
+int b = 3;
+::swap( a, b );
+std::cout << "a = " << a << ", b = " << b << std::endl;
+std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+std::string c = "chaine1";
+std::string d = "chaine2";
+::swap(c, d);
+std::cout << "c = " << c << ", d = " << d << std::endl;
+std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+return 0;
+}

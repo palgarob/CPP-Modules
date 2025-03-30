8bfbde68e13a892dad20885b2b4c9c41cec265f0
Pedro <pedpalgarob@gmail.com>
2025-03-30 20:50:19 +0000

    mo06

--- mo06/ex00/ScalarConverter.cpp
+++ mo06/ex00/ScalarConverter.cpp
@@ -6,105 +6,97 @@
 /*   By: pepaloma <pepaloma@student.42urduliz.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/18 11:46:22 by pepaloma          #+#    #+#             */
-/*   Updated: 2025/03/21 15:20:25 by pepaloma         ###   ########.fr       */
+/*   Updated: 2025/03/30 20:48:04 by pepaloma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ScalarConverter.hpp"
 #include <cstdlib>
 #include <limits>
-#include <sstream>
+#include <cerrno>
+#include <cmath>
 
-void	impossible()
+static void printInt(int i)
 {
-	std::cout << "char:		impossible" << std::endl;
-	std::cout << "int:		impossible" << std::endl;
-	std::cout << "float:		impossible" << std::endl;
-	std::cout << "double:		impossible" << std::endl;
+	if (i >= 33 && i <= 126)
+		std::cout << "char:		" << static_cast<char>(i) << std::endl;
+	else if (
+		i >= std::numeric_limits<unsigned char>::min()
+		&& i <= std::numeric_limits<unsigned char>::max()
+	)
+		std::cout << "char:		Non displayable" << std::endl;
+	else
+		std::cout << "char:		impossible" << std::endl;
+	std::cout << static_cast<int>(std::numeric_limits<unsigned char>::min());
+	std::cout << "int:		" << i << std::endl;
+	std::cout << "float:		" << static_cast<float>(i) << ".0" << "f" << std::endl;
+	std::cout << "double:		" << static_cast<double>(i) << ".0" << std::endl;
 }
 
-void	output(int integer, float floating, double doublePrecision)
+static void printFloat(float i)
 {
-	if (integer < 33 || integer > 126)
-		std::cout << "char:		Non displayable" << std::endl;
+	if (i >= 33 && i <= 126)
+		std::cout << "char:		" << static_cast<char>(i) << std::endl;
 	else if (
-		integer > std::numeric_limits<char>::max()
-		|| integer < std::numeric_limits<char>::min()
+		i >= std::numeric_limits<char>::min()
+		&& i <= std::numeric_limits<char>::max()
 	)
-		std::cout << "char:		impossible" << std::endl;
+		std::cout << "char:		Non displayable" << std::endl;
 	else
-		std::cout << "char:		" << static_cast<char>(integer) << std::endl;
-	std::cout << "int:		" << integer << std::endl;
-	std::cout << "float:		" << floating 
-	<< (std::abs(floating - static_cast<int>(floating)) > 0 ? "" : ".0") << "f" << std::endl;
-	std::cout << "double:		" << doublePrecision
-	<< (std::abs(floating - static_cast<int>(floating)) > 0 ? "" : ".0") << std::endl;
+		std::cout << "char:		impossible" << std::endl;
+	std::cout << "int:		" << static_cast<int>(i) << std::endl;
+	std::cout << "float:		" << i << (std::fmod(i, 1.0) == 0 ? ".0" : "") << "f" << std::endl;
+	std::cout << "double:		" << static_cast<double>(i) << (std::fmod(i, 1.0) == 0 ? ".0" : "") << std::endl;
+}
+
+static void printDouble(float i)
+{
+	if (i >= 33 && i <= 126)
+		std::cout << "char:		" << static_cast<char>(i) << std::endl;
+	else if (
+		i >= std::numeric_limits<unsigned char>::min()
+		&& i <= std::numeric_limits<unsigned char>::max()
+	)
+		std::cout << "char:		Non displayable" << std::endl;
+	else
+		std::cout << "char:		impossible" << std::endl;
+	std::cout << "int:		" << static_cast<int>(i) << std::endl;
+	std::cout << "float:		" << static_cast<float>(i) << (std::fmod(i, 1.0) == 0 ? ".0" : "") << "f" << std::endl;
+	std::cout << "double:		" << i << (std::fmod(i, 1.0) == 0 ? ".0" : "") << std::endl;
 }
 
 void ScalarConverter::convert(const std::string& input)
 {
 	char* endptr;
-	double num = strtod(input.c_str(), &endptr);
-	if (endptr == input.c_str()) {
-		impossible();
+	errno = 0;
+	double dou = strtod(input.c_str(), &endptr);
+	if (errno == ERANGE || endptr == input.c_str()) {
+		std::cout << "char:		impossible" << std::endl;
+		std::cout << "int:		impossible" << std::endl;
+		std::cout << "float:		impossible" << std::endl;
+		std::cout << "double:		impossible" << std::endl;
 	}
-	else if (input.find('.', 0) != std::string::npos) {
+	else if (input.find('.', 0) == std::string::npos)
+	{
 		if (
-			*endptr == 'f' && num >= std::numeric_limits<float>::min()
-			&& num <= std::numeric_limits<float>::max()
-		)
-		{
-			output(
-				static_cast<int>(num),
-				num, static_cast<double>(num)
-			);
+			dou <= std::numeric_limits<int>::max()
+			&& dou >= std::numeric_limits<int>::min()
+		) {
+			int i = std::strtol(input.c_str(), &endptr, 10);
+			printInt(i);
 		}
-		else
-		{
-			output(
-				static_cast<int>(num),
-				static_cast<float>(num), num
-			);
+		else {
+			printDouble(dou);
 		}
 	}
-	else if (
-		!input.compare(0, 4, "-inf") || !input.compare(0, 4, "+inf")
-		|| !input.compare(0, 3, "nan")
-	)
-	{
-		std::cout << "char:		impossible" << std::endl;
-		std::cout << "int:		impossible" << std::endl;
-		std::cout << "float:	"
-		<< ('f' == (char)input.at(4) ? input.substr(0, 4) : input.substr(0, 3)) << "f" << std::endl;
-		std::cout << "double:	"
-		<< ((input.at(2) == 'i') ? input.substr(0, 4) : input.substr(0, 3)) << std::endl;
-	}
-	else if (
-		!input.compare(0, 5, "-inff") || !input.compare(0, 5, "+inff")
-		|| !input.compare(0, 4, "nanf")
-	)
-	{
-		std::cout << "char:		impossible" << std::endl;
-		std::cout << "int:		impossible" << std::endl;
-		std::cout << "float:	"
-		<< ((input.at(2) == 'i') ? input.substr(0, 5) : input.substr(0, 4)) << std::endl;
-		std::cout << "double:	"
-		<< ((input.at(2) == 'i') ? input.substr(0, 4) : input.substr(0, 3)) << std::endl;
-	}
-	else
-	{
-		long long	num;
-		std::stringstream strs(input);
-		strs >> num;
-		if (num < std::numeric_limits<int>::min() || num > std::numeric_limits<int>::max())
-			impossible();
-		else
-		{
-			output(
-				static_cast<int>(num),
-				static_cast<float>(num),
-				static_cast<double>(num)
-			);
+	else {
+		errno = 0;
+		float flo = strtof(input.c_str(), &endptr);
+		if (*endptr == 'f' && errno == 0) {
+			printFloat(flo);
+		}
+		else {
+			printDouble(dou);
 		}
 	}
 }

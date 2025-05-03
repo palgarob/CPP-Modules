5ef76e2bf8ba61b23446992ec451f690a028a4ca
Pedro <pedpalgarob@gmail.com>
2025-05-03 17:50:02 +0000

    fix mo09/ex02

--- mo09/ex02/main.cpp
+++ mo09/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: pepaloma <pepaloma@student.42urduliz.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/01 13:55:00 by pepaloma          #+#    #+#             */
-/*   Updated: 2025/05/03 16:49:58 by pepaloma         ###   ########.fr       */
+/*   Updated: 2025/05/03 17:43:45 by pepaloma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,20 +19,29 @@
 #include <algorithm>
 #include <cstdlib>
 #include "PmergeMe.hpp"
+#include <limits>
 
-//static long g_list_time = 0;
+static long g_list_time = 0;
 static long g_vector_time = 0;
 
 void parse_list(const std::vector<std::string> args, PmergeMe::List& v)
 {
 	std::stringstream ss;
-	unsigned n;
+	long long n;
 	for (std::size_t i = 0; i < args.size(); i++)
 	{
 		ss.clear(); ss.str(args[i]); ss >> n;
-		if (ss.fail())
+		if (ss.fail() || !(ss.eof()))
 		{
 			std::cerr << "Error: Wrong input format\n" << args[i] << std::endl;
+			exit(1);
+		}
+		if (
+			n > std::numeric_limits<unsigned>::max()
+			|| n < std::numeric_limits<unsigned>::min()
+		)
+		{
+			std::cerr << "Error: range should be inside unsigned int " << args[i] << std::endl;
 			exit(1);
 		}
 		v.push_back(n);
@@ -42,7 +51,7 @@ void parse_list(const std::vector<std::string> args, PmergeMe::List& v)
 void parse_vector(const std::vector<std::string> args, PmergeMe::Vector& v)
 {
 	std::stringstream ss;
-	unsigned n;
+	long long n;
 	for (std::size_t i = 0; i < args.size(); i++)
 	{
 		ss.clear(); ss.str(args[i]); ss >> n;
@@ -83,7 +92,7 @@ PmergeMe::List with_list(std::vector<std::string> args)
 		std::cout << result[i] << std::endl;
 	} */
 	std::clock_t end = std::clock();
-	g_vector_time = static_cast<long>((static_cast<double>(end - start) / CLOCKS_PER_SEC) * 1e6);
+	g_list_time = static_cast<long>((static_cast<double>(end - start) / CLOCKS_PER_SEC) * 1e6);
 	return (result);
 }
 
@@ -104,9 +113,9 @@ int main(int argc, char **argv)
 	PmergeMe::List l_unsorted; parse_list(args, l_unsorted);
 	PmergeMe::Vector v_sorted = with_vector(args);
  	std::cout << "Before:	"; PmergeMe::printNumbers(v_unsorted); std::cout << std::endl;
-	std::cout << "Before:	"; PmergeMe::printNumbers(l_unsorted); std::cout << std::endl;
-	std::cout << "After:	"; PmergeMe::printNumbers(v_sorted); std::cout << std::endl;
+//	std::cout << "Before:	"; PmergeMe::printNumbers(l_unsorted); std::cout << std::endl;
+//	std::cout << "After:	"; PmergeMe::printNumbers(v_sorted); std::cout << std::endl;
 	std::cout << "After:	"; PmergeMe::printNumbers(l_sorted); std::cout << std::endl;
 	std::cout << "Time to process a range of " << v_sorted.size() << " elements with a vector: " << g_vector_time << " us" << std::endl;
-	std::cout << "Time to process a range of " << l_sorted.size() << " elements with a list: " << g_vector_time << " us" << std::endl;
+	std::cout << "Time to process a range of " << l_sorted.size() << " elements with a list: " << g_list_time << " us" << std::endl;
 }
